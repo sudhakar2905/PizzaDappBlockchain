630916b966e37de6754a4a9339dc6c2b81a569e8
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-09-12 17:49:39 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,270 @@
-import pizzaABI from "./contract/pizza.json"; // assuming you have a pizza contract ABI
-import { useState, useEffect } from "react";
-import { ethers } from "ethers";
-import BuyPizza from "./components/BuyPizza"; // a component for buying pizza
-import PizzaMemos from "./components/PizzaMemos"; // a component for pizza memos
-import pizzaImage from "./pizza.jpeg"; // an image of pizza
-import "./App.css";
+const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "pizzaIndex",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "toppings",
+				"type": "string"
+			}
+		],
+		"name": "buyPizza",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "pizzaName",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "toppings",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			}
+		],
+		"name": "PizzaOrdered",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "availablePizzas",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAvailablePizzas",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Pizza.PizzaDetails[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getOrders",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "pizzaName",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "toppings",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "timestamp",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "buyer",
+						"type": "address"
+					}
+				],
+				"internalType": "struct Pizza.PizzaOrder[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "orders",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "pizzaName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "toppings",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+const contractAddress = '0xf9DE8Df1C750d165A6312B8c0EdE880e912295F5';  // Replace with your deployed contract address
 
-function App() {
-  const [state, setState] = useState({
-    provider: null,
-    signer: null,
-    contract: null,
-  });
-  const [account, setAccount] = useState("None");
-  useEffect(() => {
-    const connectWallet = async () => {
-      const contractAddress = "0xYourPizzaContractAddress"; // Replace with your pizza contract address
-      const contractABI = pizzaABI.abi; // assuming your pizza contract ABI is stored in pizza.json
-      try {
-        const { ethereum } = window;
+let web3;
+let contract;
 
-        if (ethereum) {
-          const account = await ethereum.request({
-            method: "eth_requestAccounts",
-          });
+async function loadBlockchainData() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        contract = new web3.eth.Contract(contractABI, contractAddress);
 
-          window.ethereum.on("chainChanged", () => {
-            window.location.reload();
-          });
-
-          window.ethereum.on("accountsChanged", () => {
-            window.location.reload();
-          });
-
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(
-            contractAddress,
-            contractABI,
-            signer
-          );
-          setAccount(account);
-          setState({ provider, signer, contract });
-        } else {
-          alert("Please install Metamask");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    connectWallet();
-  }, []);
-  // console.log(state);
-  return (
-    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
-      <img src={pizzaImage} className="img-fluid" alt="Pizza" width="100%" />
-      <p className="text-muted lead" style={{ marginTop: "10px", marginLeft: "5px" }}>
-        <small>Connected Account - {account}</small>
-      </p>
-      <div className="container">
-        <BuyPizza state={state} />
-        <PizzaMemos state={state} />
-      </div>
-    </div>
-  );
+        // Load available pizzas
+        loadPizzas();
+        
+        // Display order history
+        displayOrders();
+    } else {
+        alert("Please install MetaMask to use this DApp!");
+    }
 }
 
-export default App;
+async function loadPizzas() {
+    const pizzas = await contract.methods.getAvailablePizzas().call();
+    const pizzaSelect = document.getElementById('pizza');
+    pizzaSelect.innerHTML = '';  // Clear existing options
+
+    pizzas.forEach((pizza, index) => {
+        const option = document.createElement('option');
+        option.value = index;
+        option.text = `${pizza.name} - ${web3.utils.fromWei(pizza.price, 'ether')} ETH`;
+        pizzaSelect.appendChild(option);
+    });
+}
+
+async function buyPizza() {
+    const pizzaIndex = document.getElementById('pizza').value;
+    const toppings = document.getElementById('toppings').value;
+    const accounts = await web3.eth.getAccounts();
+    
+    const pizza = await contract.methods.getAvailablePizzas().call(pizzaIndex);
+    const pizzaPrice = pizza[pizzaIndex].price;
+
+    // Send transaction
+    await contract.methods.buyPizza(pizzaIndex, toppings).send({
+        from: accounts[0],
+        value: pizzaPrice
+    });
+
+    // Reload order history
+    displayOrders();
+}
+
+async function displayOrders() {
+    const orders = await contract.methods.getOrders().call();
+    const ordersList = document.getElementById('orders');
+    ordersList.innerHTML = '';  // Clear the existing list
+
+    orders.forEach(order => {
+        const li = document.createElement('li');
+        li.innerHTML = `Pizza: ${order.pizzaName}, Toppings: ${order.toppings}, Price: ${web3.utils.fromWei(order.price, 'ether')} ETH, Buyer: ${order.buyer}`;
+        ordersList.appendChild(li);
+    });
+}
+
+document.getElementById('buyPizzaBtn').addEventListener('click', buyPizza);
+
+window.onload = loadBlockchainData;
