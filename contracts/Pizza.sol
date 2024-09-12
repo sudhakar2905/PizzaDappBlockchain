58655e49eb893c91b4f5f15b18a92c14674c1db3
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2024-09-12 17:47:33 +0530

    Update Pizza.sol

--- contracts/Pizza.sol
+++ contracts/Pizza.sol
@@ -2,27 +2,72 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Pizza {
-    struct Memo {
-        string name;
+    
+    struct PizzaOrder {
+        string pizzaName;
         string toppings;
+        uint256 price;  // Price in wei (smallest denomination of Ether)
         uint256 timestamp;
-        address from;
+        address buyer;
     }
 
-    Memo[] memos;
-    address payable owner;
+    struct PizzaDetails {
+        string name;
+        uint256 price;
+    }
+
+    PizzaDetails[] public availablePizzas;  // List of pizzas available
+    PizzaOrder[] public orders;  // List of all orders
+
+    address payable public owner;  // Contract owner (pizza shop)
+
+    event PizzaOrdered(
+        string pizzaName,
+        string toppings,
+        uint256 price,
+        uint256 timestamp,
+        address buyer
+    );
 
     constructor() {
         owner = payable(msg.sender);
+
+        // Add some pizza varieties with prices
+        availablePizzas.push(PizzaDetails("Margherita", 0.01 ether));
+        availablePizzas.push(PizzaDetails("Pepperoni", 0.015 ether));
+        availablePizzas.push(PizzaDetails("Veggie", 0.012 ether));
+        availablePizzas.push(PizzaDetails("BBQ Chicken", 0.018 ether));
     }
 
-    function buyPizza(string memory name, string memory toppings) public payable {
-        require(msg.value > 0, "Please pay greater than 0 ether");
+    // Function to buy a pizza
+    function buyPizza(uint256 pizzaIndex, string memory toppings) public payable {
+        require(pizzaIndex < availablePizzas.length, "Invalid pizza selection");
+        PizzaDetails memory pizza = availablePizzas[pizzaIndex];
+        require(msg.value == pizza.price, "Incorrect payment amount");
+
+        // Transfer the payment to the owner
         owner.transfer(msg.value);
-        memos.push(Memo(name, toppings, block.timestamp, msg.sender));
+
+        // Record the transaction
+        orders.push(PizzaOrder(
+            pizza.name,
+            toppings,
+            pizza.price,
+            block.timestamp,
+            msg.sender
+        ));
+
+        // Emit an event for the new pizza order
+        emit PizzaOrdered(pizza.name, toppings, pizza.price, block.timestamp, msg.sender);
     }
 
-    function getMemos() public view returns (Memo[] memory) {
-        return memos;
+    // Get the list of available pizzas
+    function getAvailablePizzas() public view returns (PizzaDetails[] memory) {
+        return availablePizzas;
+    }
+
+    // Get all orders (transaction history)
+    function getOrders() public view returns (PizzaOrder[] memory) {
+        return orders;
     }
 }
